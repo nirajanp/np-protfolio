9178a50dac713e58fba21e736f25ac664e1b4a99
Nirajan Pandey <nirajanp@dhcp-wifi-8021x-155-41-105-132.bu.edu>
2021-04-13 11:49:34 -0400

    media queries and minor changes

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+const navItem = document.getElementsByClassName('nav-link');
+
+// const navItemHandler = () => {
+//   document.getElementsById('nav-link').style.display = 'none';
+// }
+
+// navItem.addEventListener('click', navItemHandler);
